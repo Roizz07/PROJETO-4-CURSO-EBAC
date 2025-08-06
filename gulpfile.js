c77b6c649da7f29040eb8d943d3a51de2a7826ff
davi <davi@email.com>
2025-08-05 21:54:45 -0400

    inicio da criação do hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagein = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/styles/**/*.scss')
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/styles'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagein())
+        .pipe(gulp.dest('./dist/images'));
+        
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
-};
+}
